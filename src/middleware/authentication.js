01a0706f5b199e3f0acabf26149e239700f6a559
Vann <vannytjahyadi@gmail.com>
2018-07-07 22:33:30 +0700

    remove unused js files

--- src/middleware/authentication.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.tokenGuard = (function () { return function (req, res, next) {
-    console.log(req);
-    console.log('check token');
-    next();
-}; });
--- src/routes/api/v1/auth.router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var Authentication_controller_1 = require("@controller/Authentication.controller");
-var Register_service_1 = require("@request/authentication/Register.service");
-exports.authRouter = express_1.Router();
-var authentication = new Authentication_controller_1.AuthenticationController();
-var registerRequestService = new Register_service_1.RegisterRequestService();
-exports.authRouter.post('/register', registerRequestService.register, authentication.register);
