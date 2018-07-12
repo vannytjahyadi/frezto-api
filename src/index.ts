8f77c87d2fd87c7209a09be0feb52f17e753405e
vannytjahyadi <vannytjahyadi@gmail.com>
2018-07-12 11:07:20 +0700

    set port

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ import dbConfig from './config/Database';
 createConnection(dbConfig).then(async connection => {
 
     const app = express();
-    const port = process.env.port || 8080;
+    const port = process.env.PORT || 8080;
 
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
